a9875c0fbf6ff52000a50d5fa19590f44d696df7
anais <anaiskg@outlook.fr>
2026-04-03 14:32:09 +0200

    init(): creation des attributs de la classes utilisateur

--- src/modele/Utilisateur.php
+++ src/modele/Utilisateur.php
@@ -2,7 +2,17 @@
 
 namespace modele;
 
-class Utilisateur
-{
+class Utilisateur{
+    private $idUtilisateur;
+    private $nom;
+    private $prenom;
+    private $dateNaissance;
+    private $email;
+    private $telephone;
+    private $adresse;
+    private $motDePasse;
+    private $status;
+    private $gestion;
+
 
 }
