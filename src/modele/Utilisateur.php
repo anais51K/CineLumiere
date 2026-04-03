899cd5ec0728d0f843ea1447f9ef97996530017c
anais <anaiskg@outlook.fr>
2026-04-03 14:40:16 +0200

    refactor(): creation constructeur de la classe utilisateur

--- src/modele/Utilisateur.php
+++ src/modele/Utilisateur.php
@@ -14,5 +14,31 @@ class Utilisateur{
     private $status;
     private $gestion;
 
+    /**
+     * @param $idUtilisateur
+     * @param $nom
+     * @param $prenom
+     * @param $dateNaissance
+     * @param $email
+     * @param $telephone
+     * @param $adresse
+     * @param $motDePasse
+     * @param $status
+     * @param $gestion
+     */
+    public function __construct($idUtilisateur, $nom, $prenom, $dateNaissance, $email, $telephone, $adresse, $motDePasse, $status, $gestion)
+    {
+        $this->idUtilisateur = $idUtilisateur;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->dateNaissance = $dateNaissance;
+        $this->email = $email;
+        $this->telephone = $telephone;
+        $this->adresse = $adresse;
+        $this->motDePasse = $motDePasse;
+        $this->status = $status;
+        $this->gestion = $gestion;
+    }
+
 
 }
