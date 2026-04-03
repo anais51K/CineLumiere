b60dd1106a109947044d19bd2d55197df60d589c
anais <anaiskg@outlook.fr>
2026-04-03 14:43:07 +0200

    refactor(): creation getter de la classe film

--- src/modele/Film.php
+++ src/modele/Film.php
@@ -37,5 +37,79 @@ class Film{
         $this->idFilm = $idFilm;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getIdFilm()
+    {
+        return $this->idFilm;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAffiche()
+    {
+        return $this->affiche;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAgeMin()
+    {
+        return $this->age_min;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRealisateur()
+    {
+        return $this->realisateur;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateSortie()
+    {
+        return $this->dateSortie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBandeAnnonce()
+    {
+        return $this->bandeAnnonce;
+    }
+
+
+
 
 }
