d4a37225ca6b0678b22e0619faa93112e95cfe7a
anais <anaiskg@outlook.fr>
2026-04-03 14:38:21 +0200

    refactor(): creation constructeur de la classe reservation

--- src/modele/Reservation.php
+++ src/modele/Reservation.php
@@ -11,4 +11,25 @@ class Reservation{
     private $statut;
     private $modePaiement;
 
+    /**
+     * @param $idReservation
+     * @param $nbPlacesSenior
+     * @param $nbPlacesEtudiant
+     * @param $nbPlacesAdulte
+     * @param $etat
+     * @param $statut
+     * @param $modePaiement
+     */
+    public function __construct($idReservation, $nbPlacesSenior, $nbPlacesEtudiant, $nbPlacesAdulte, $etat, $statut, $modePaiement)
+    {
+        $this->idReservation = $idReservation;
+        $this->nbPlacesSenior = $nbPlacesSenior;
+        $this->nbPlacesEtudiant = $nbPlacesEtudiant;
+        $this->nbPlacesAdulte = $nbPlacesAdulte;
+        $this->etat = $etat;
+        $this->statut = $statut;
+        $this->modePaiement = $modePaiement;
+    }
+
+
 }
