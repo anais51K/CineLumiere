225f246761fe8c5ae667cf1c5dd2a854a4b4c0e3
anais <anaiskg@outlook.fr>
2026-04-03 14:39:38 +0200

    refactor(): creation constructeur de la classe seance

--- src/modele/Seance.php
+++ src/modele/Seance.php
@@ -7,5 +7,17 @@ class Seance{
     private $date;
     private $etat;
 
+    /**
+     * @param $idSeance
+     * @param $date
+     * @param $etat
+     */
+    public function __construct($idSeance, $date, $etat)
+    {
+        $this->idSeance = $idSeance;
+        $this->date = $date;
+        $this->etat = $etat;
+    }
+
 
 }
