afbec6d95cf5529b40ed1fc5c66b63db4bed13ea
anais <anaiskg@outlook.fr>
2026-04-03 14:38:55 +0200

    refactor(): creation constructeur de la classe salle

--- src/modele/Salle.php
+++ src/modele/Salle.php
@@ -8,5 +8,19 @@ class Salle{
     private $code;
     private $etat;
 
+    /**
+     * @param $idSalle
+     * @param $capaciteMax
+     * @param $code
+     * @param $etat
+     */
+    public function __construct($idSalle, $capaciteMax, $code, $etat)
+    {
+        $this->idSalle = $idSalle;
+        $this->capaciteMax = $capaciteMax;
+        $this->code = $code;
+        $this->etat = $etat;
+    }
+
 
 }
