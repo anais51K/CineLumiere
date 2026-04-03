990b93e5d670545d02527ed0cbd0a5a0e3e06411
anais <anaiskg@outlook.fr>
2026-04-03 14:06:59 +0200

    init(): creation des autres fichiers

--- src/bdd/Bdd.php
+++ src/bdd/Bdd.php
@@ -2,5 +2,20 @@
 
 class Bdd
 {
+    private $connexionBdd;
+    private $identifiant = "root";
+    private $motDePasse = "";
+    private $nomBdd = "cine_lumiere";
+    private $host = "localhost";
+
+    public function __construct()
+    {
+        $this->connexionBdd = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->nomBdd, $this->identifiant, $this->motDePasse);
+    }
+
+    public function getConnexionBdd()
+    {
+        return $this->connexionBdd;
+    }
 
 }
---
+++ src/traitement/NewCodePromo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace traitement;
+
+class NewCodePromo{
+require_once '../bdd/Bdd.php';
+require_once '../modele/CodePromo.php';
+
+
+if(isset($_POST['codePromo']) && isset($_POST['pourcentageReduction'])){
+$codePromo = new CodePromo(null, $_POST['codePromo'], $_POST['pourcentageReduction'], true);
+}
