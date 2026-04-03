93f60c7f1c22a399e187110d31dd2690937bfdde
Younes95140 <younes051@outlook.fr>
2026-04-03 16:16:10 +0200

    Creation de la page Repository Reservation et Utilisateur

--- src/repository/ReservationRepository.php
+++ src/repository/ReservationRepository.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace repository;
+
+class ReservationRepository
+{
+    private $connexionBdd;
+    public function __construct()
+    {
+        $this->connexionBdd = (new Bdd())->getConnexionBdd();
+    }
+    Public function GetReservation(idRersveration)
+    {
+
+    }
+
+}
