ad6ea8f7b6dfd4b4ec1363fc4e073df3d70d77fe
anais <anaiskg@outlook.fr>
2026-04-03 14:37:37 +0200

    refactor(): creation constructeur de la classe film

--- src/modele/Film.php
+++ src/modele/Film.php
@@ -13,4 +13,29 @@ class Film{
     private $dateSortie;
     private $bandeAnnonce;
 
+    /**
+     * @param $bandeAnnonce
+     * @param $dateSortie
+     * @param $realisateur
+     * @param $age_min
+     * @param $genre
+     * @param $affiche
+     * @param $duree
+     * @param $nom
+     * @param $idFilm
+     */
+    public function __construct($bandeAnnonce, $dateSortie, $realisateur, $age_min, $genre, $affiche, $duree, $nom, $idFilm)
+    {
+        $this->bandeAnnonce = $bandeAnnonce;
+        $this->dateSortie = $dateSortie;
+        $this->realisateur = $realisateur;
+        $this->age_min = $age_min;
+        $this->genre = $genre;
+        $this->affiche = $affiche;
+        $this->duree = $duree;
+        $this->nom = $nom;
+        $this->idFilm = $idFilm;
+    }
+
+
 }
