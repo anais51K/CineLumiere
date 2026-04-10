95a05b2d7eb80154ffb43d4eaf0600bfbda20a5a
anais <anaiskg@outlook.fr>
2026-04-10 14:36:03 +0200

    refactor(): modification du code avec ajout de supprimer et modifier film

--- src/repository/FilmRepository.php
+++ src/repository/FilmRepository.php
@@ -4,35 +4,43 @@ namespace repository;
 
 use modele\Film;
 
-class FilmRepository{
+class FilmRepository
+{
     private $connexionBdd;
+
     public function __construct()
     {
         $this->connexionBdd = (new Bdd())->getConnexionBdd();
     }
-    public function getFilm($idFilm){
+
+    public function getFilm($idFilm)
+    {
         $sql = "SELECT * FROM Film WHERE idfilm = :idfilm";
         $req = $this->connexionBdd->prepare($sql);
         $req->bindValue(':idFilm', $idFilm);
         $req->execute();
         $result = $req->fetch();
-        $film = new Film($result["id_film"],$result["nom"],$result["duree"],$result["affiche"]);$result["genre"],$result["age_min"],$result["realisateur"],$result["date_sortie"],$result["bande_annonce"];
+        $film = new Film ($result["id_film"], $result["nom"], $result["duree"], $result["affiche"], $result["genre"],$result["age_min"],$result["realisateur"],$result["date_sortie"],$result["bande_annonce"]);
         return $film;
     }
-    public function getAllFilm(){
+
+    public function getAllFilm()
+    {
         $sql = "SELECT * FROM Film";
         $req = $this->connexionBdd->prepare($sql);
         $req->execute();
         $results = $req->fetchAll();
         $tabFilm = array();
         foreach ($results as $result) {
-            $film = new Film($result["id_film"],$result["nom"],$result["duree"],$result["affiche"],$result["genre"],$result["age_min"],$result["realisateur"],$result["date_sortie"],$result["bande_annonce"]);
+            $film = new Film($result["id_film"], $result["nom"], $result["duree"], $result["affiche"], $result["genre"], $result["age_min"], $result["realisateur"], $result["date_sortie"], $result["bande_annonce"]);
             $tabFilm[] = $film;
         }
-        return $tabCodePromo;
+        return $tabFilm;
     }
-    public function ajouterFilm(Film $film){
-        $sql= "";
+
+    public function ajouterFilm(Film $film)
+    {
+        $sql = "";
         $req = $this->connexionBdd->prepare($sql);
         $req->bindValue(':nom', $film->getNom());
         $req->bindValue(':duree', $film->getDuree());
@@ -43,5 +51,28 @@ class FilmRepository{
         $req->bindValue(':date_sortie', $film->getDateSortie());
         $req->bindValue(':bande_annonce', $film->getBandeAnnonce());
         $req->execute();
+
+    }
+
+    public function supprimerFilm($idFilm){
+        $sql = "DELETE FROM Film WHERE idfilm = :idfilm";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->bindValue(':idfilm', $idFilm);
+        $req->execute();
+    }
+    public function modifierFilm(Film $film){
+        $sql = "";
+        $req = $this->connexionBdd->prepare($sql);
+        $req->bindValue(':nom', $film->getNom());
+        $req->bindValue(':duree', $film->getDuree());
+        $req->bindValue(':affiche', $film->getAffiche());
+        $req->bindValue(':genre', $film->getGenre());
+        $req->bindValue(':age_min', $film->getAgeMin());
+        $req->bindValue(':realisateur', $film->getRealisateur());
+        $req->bindValue(':date_sortie', $film->getDateSortie());
+        $req->bindValue(':bande_annonce', $film->getBandeAnnonce());
+        $req->execute();
+
+    }
 
 }
